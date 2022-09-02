bfba66aec25e8b07c135cec9fba4656b301d4e6d
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-02 21:24:35 +0300

    commit tree 4bab2546137e3dfc67c7d4abfb3983bb23cbd9be

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,25 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+      # def full_name
+      #   "#{self.first_name} #{self.last_name}" 
+      # end
+      # def list_roles
+      #     # names = self.characters.pluck(:full_name)
+      #     # shows = self.show.pluck(:name)
+      #     # names << shows
+      #     self.characters.map do |character|
+      #       "#{character.name} - #{character.show.name}"
+      #     end
+      #   end
+
+      def full_name
+        "#{self.first_name} #{self.last_name}"
+      end
+    
+      def list_roles
+        self.characters.map do |character|
+          "#{character.name} - #{character.show.name}"
+        end
+      end
 end
