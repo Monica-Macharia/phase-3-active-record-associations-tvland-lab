182ad30c381f731df1ff77c7c46b81e7e0d93fb3
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-02 21:25:18 +0300

    associations

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,20 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+    # def characters
+    #     Character.all
+    # end
+    # def network
+    #     self.network.find()
+    # end
+    # def actors_list
+        
+    # end
+
+  def actors_list
+    self.actors.map do |actor|
+      actor.full_name
+    end
+  end
 end
